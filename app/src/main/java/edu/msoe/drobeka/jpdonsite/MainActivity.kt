f3b28b5defc88c16345f221fe5da7dcbb43d1d54
odrobek <odrobek@gmail.com>
2023-11-15 18:12:13 -0600

    Initial Commit

---
+++ app/src/main/java/edu/msoe/drobeka/jpdonsite/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.msoe.drobeka.jpdonsite
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
